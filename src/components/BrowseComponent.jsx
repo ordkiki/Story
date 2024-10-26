7ccd6515a9dd922597c0c88c2809ca5c007aa34f
ordkiki <onjaniaina235497@gmail.com>
2024-10-26 15:32:13 +0300

    maj Click collection

--- src/components/BrowseComponent.jsx
+++ src/components/BrowseComponent.jsx
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { CardRead, CardListen } from './CardComponent';
-import { FaAngleRight } from 'react-icons/fa';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { CardRead, CardListen } from "./CardComponent";
+import { FaAngleRight } from "react-icons/fa";
 import axios from "axios";
-
 
 export function Listen() {
   return (
-    <div className='listen my-10'>
-      <div className='flex flex-col md:flex-row justify-between items-center'>
-        <h3 className='font-medium text-2xl'>Listen</h3>
-        <Link to="/ListenMore" className='flex items-center mt-2 md:mt-0'>
-          Voir plus <FaAngleRight className='ml-1' />
+    <div className="listen my-10">
+      <div className="flex flex-col md:flex-row justify-between items-center">
+        <h3 className="font-medium text-2xl">Listen</h3>
+        <Link to="/ListenMore" className="flex items-center mt-2 md:mt-0">
+          Voir plus <FaAngleRight className="ml-1" />
         </Link>
       </div>
-      <div className='p-4 card-vakiana flex flex-wrap justify-between'>
+      <div className="p-4 card-vakiana flex flex-wrap justify-between">
         <CardListen title="Angatra" description="tantara mapatahoatra" />
         <CardListen title="Angatra" description="tantara mapatahoatra" />
         <CardListen title="Angatra" description="tantara mapatahoatra" />
@@ -31,8 +30,8 @@ export function Read() {
     try {
       const response = await axios.get("http://127.0.0.1:8001/Histories/Read");
       if (response.data.status === "success") {
-        console.log("Résultats:", response.data.results[0]); 
-        setData(response.data.results[0]); 
+        console.log("Résultats:", response.data.results[0]);
+        setData(response.data.results[0]);
       }
     } catch (error) {
       console.error("Erreur lors de la requête:", error);
@@ -44,17 +43,24 @@ export function Read() {
   }, []);
 
   return (
-    <div className='read my-5'>
-      <div className='flex flex-col md:flex-row justify-between items-center mb-4'>
-        <h3 className='font-medium text-xl md:text-2xl'>Read</h3>
-        <Link to="/ReadMore" className='flex items-center mt-2 md:mt-0 text-sm md:text-base'>
-          Voir plus <FaAngleRight className='ml-1' />
+    <div className="read my-5">
+      <div className="flex flex-col md:flex-row justify-between items-center mb-4">
+        <h3 className="font-medium text-xl md:text-2xl">Read</h3>
+        <Link
+          to="/ReadMore"
+          className="flex items-center mt-2 md:mt-0 text-sm md:text-base"
+        >
+          Voir plus <FaAngleRight className="ml-1" />
         </Link>
       </div>
-      <div className='p-2 card-vakiana flex flex-wrap justify-between'>
+      <div className="p-2 card-vakiana flex flex-wrap justify-between">
         {data.length > 0 ? (
           data.map((donne) => (
-            <CardRead key={donne.id_histories} title={donne.title || "Titre par défaut"} description={donne.content || "Description par défaut"} />
+            <CardRead
+              key={donne.id_histories}
+              title={donne.title || "Titre par défaut"}
+              description={donne.content || "Description par défaut"}
+            />
           ))
         ) : (
           <p>Aucune donnée à afficher.</p>
--- src/components/ListeComponent.jsx
+++ src/components/ListeComponent.jsx
@@ -1,26 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import List from './listModel.jsx'
 import images from "../assets/image/LOGO.png"
 function ListComponent() {
-  return (
+    const [afficheRead,setAfficheRead] = useState(true)
+    const [afficheListen,setAfficheListen] = useState(false)
+    function ListeRead(){
+        setAfficheRead(true)
+        setAfficheListen(false)
+        console.log(afficheRead);
+        
+    }
+    function ListeListen(){
+        setAfficheRead(false)
+        setAfficheListen(true)
+    }
+    return (
     <div>
         <div className='p-4 mx-36 flex items-center justify-between'>
             <h1 className='font-bold text-3xl'>My contents</h1>
             <div className='read'>
-                <button className='font-bold bg-black px-[1.7vw] py-[0.5vw] text-white hover:bg-slate-800 rounded-[10px]'>Create Story</button>
+                <button 
+                    
+                    className='font-bold bg-black px-[1.7vw] py-[0.5vw] text-white hover:bg-slate-800 rounded-[10px]'>
+                        Create Story
+                </button>
             </div>
         </div>
+        
         <div className='p-4 mx-36 flex items-center'>
-            <button className='font-bold bg-black px-[1.7vw] py-[0.5vw] text-white rounded-t-[10px]'>
+            <button
+                onClick={ListeRead} 
+                className={
+                    `font-bold px-[1.7vw] py-[0.5vw]  rounded-t-[10px] 
+                    ${
+                    afficheRead ? 'bg-black text-white' : 'bg-white text-black hover:bg-gray-800 hover:text-white'}`}>
+                  
                 Read(20)
             </button>
-            <button className='font-bold px-[1.7vw] py-[0.5vw] text-gray-700 hover:bg-slate-800 rounded-t-[10px] hover:text-white'>
+            
+            <button
+                onClick={ListeListen} 
+                className={
+                    `font-bold px-[1.7vw] py-[0.5vw] text-gray-700 rounded-t-[10px] 
+                    ${
+                    afficheListen ? 'bg-black text-white' : 'bg-white text-black hover:bg-gray-800 hover:text-white'}`}>
+   
                 Listen(0)
             </button>
         </div>
+
+
         <div className='w-screen flex justify-center items-center'>
             <hr className='border-[1px] w-[83.3vw] border-black'/>
         </div>
+
         <div className='p-4 mx-36 flex items-center justify-between'>
             <div className='flex'>
                 <h1 className='px-[2vw] font-bold'>#</h1>
@@ -34,9 +67,15 @@ function ListComponent() {
                 <h1 className='px-[2vw] font-bold'>Updated date</h1>
             </div>
         </div>
+
         <div className='w-screen flex justify-center items-center'>
             <hr className='border-[1px] w-[83.3vw] border-black'/>
         </div>
+
+        {afficheRead && 
+        <div>
+
+        
         <List
             id="1"
             image={images}
@@ -64,6 +103,8 @@ function ListComponent() {
             status="published"
             date="12 fevrier 2020"
         />
+        </div>
+    }
     </div>
   )
 }
