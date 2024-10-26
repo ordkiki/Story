99cae8068a19a6f3c2722804f21d194c6875b48c
ordkiki <onjaniaina235497@gmail.com>
2024-10-26 16:06:37 +0300

    Creation collection

---
+++ src/components/CreationStoryComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export function PopUp(){
+    return(
+        <div className='top-0 bg-[rgba(0,0,0,0.2)] w-screen h-screen flex items-center justify-center absolute'>
+            <div className='p-10 w-[40vw] h-[70vh] bg-white border rounded-[20px]'>
+                <h3 className='text-xl font-medium'>Create history</h3>
+                <div className='p-20'>
+                    <h2 className='text-center text-2xl font-bold'>Shows Us your creativity</h2>
+                    <p className='text-center text-gray-400'>Choose What type of Story you want</p>
+                    <div>
+                        <div className=''>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
+function CreationStoryComponent() {
+  return (
+    <div className=''>CreationStoryComponent</div>
+  )
+}
+
+export default CreationStoryComponent
