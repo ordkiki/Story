d0e5cfa907ece430c68ee9bcd7e6bf2a64f6dfc3
Rova <fanantenanarova50@gmail.com>
2024-10-26 12:56:19 +0200

    Ajout des collections et liste des story

--- src/views/Collection.jsx
+++ src/views/Collection.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
+import Navbar from '../components/Navbar'
+import ListComponent from '../components/ListeComponent.jsx'
 function Collection() {
   return (
-    <div>Collection</div>
+    <div>
+      <Navbar></Navbar>
+      <ListComponent/>
+    </div>
   )
 }
 
