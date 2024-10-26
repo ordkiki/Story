43629d80853b9335d7377dcdffe4917937872742
ordkiki <onjaniaina235497@gmail.com>
2024-10-26 09:36:26 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-4xl font-bold text-blue-500">Bienvenue à Tailwind avec React et Vite !</h1>
+      <p className="text-lg text-gray-700">Votre projet est prêt à utiliser Tailwind CSS.</p>
+    </div>
+  )
+}
+
+export default App
