6b65fa9e684c43571dedac85ff53058d890a9ab4
ZouFitahiana <129894768+Fitahiana-herizo-RAKOTOMAMPIONONA@users.noreply.github.com>
2024-10-26 16:27:12 +0300

    audio & histoitr dispatche

--- src/components/BrowseComponent.jsx
+++ src/components/BrowseComponent.jsx
@@ -1,30 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { CardRead, CardListen } from "./CardComponent";
-import { FaAngleRight } from "react-icons/fa";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { CardRead, CardListen } from './CardComponent';
+import { FaAngleRight } from 'react-icons/fa';
 import axios from "axios";
 
 export function Listen() {
-  return (
-    <div className="listen my-10">
-      <div className="flex flex-col md:flex-row justify-between items-center">
-        <h3 className="font-medium text-2xl">Listen</h3>
-        <Link to="/ListenMore" className="flex items-center mt-2 md:mt-0">
-          Voir plus <FaAngleRight className="ml-1" />
-        </Link>
-      </div>
-      <div className="p-4 card-vakiana flex flex-wrap justify-between">
-        <CardListen title="Angatra" description="tantara mapatahoatra" />
-        <CardListen title="Angatra" description="tantara mapatahoatra" />
-        <CardListen title="Angatra" description="tantara mapatahoatra" />
-        <CardListen title="Angatra" description="tantara mapatahoatra" />
-      </div>
-    </div>
-  );
-}
-
-export function Read() {
   const [data, setData] = useState([]);
+  const [donneesFiltres, setDonneesFiltres] = useState([]);
 
   const fetchData = async () => {
     try {
@@ -42,25 +24,67 @@ export function Read() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    setDonneesFiltres(data.filter((donne) => donne.types === "audio"));
+  }, [data]); 
+
   return (
-    <div className="read my-5">
-      <div className="flex flex-col md:flex-row justify-between items-center mb-4">
-        <h3 className="font-medium text-xl md:text-2xl">Read</h3>
-        <Link
-          to="/ReadMore"
-          className="flex items-center mt-2 md:mt-0 text-sm md:text-base"
-        >
-          Voir plus <FaAngleRight className="ml-1" />
+    <div className='listen my-10'>
+      <div className='flex flex-col md:flex-row justify-between items-center'>
+        <h3 className='font-medium text-2xl'>Listen</h3>
+        <Link to="/ListenMore" className='flex items-center mt-2 md:mt-0'>
+          Voir plus <FaAngleRight className='ml-1' />
         </Link>
       </div>
-      <div className="p-2 card-vakiana flex flex-wrap justify-between">
-        {data.length > 0 ? (
-          data.map((donne) => (
-            <CardRead
-              key={donne.id_histories}
-              title={donne.title || "Titre par défaut"}
-              description={donne.content || "Description par défaut"}
-            />
+      <div className='p-4 card-vakiana flex flex-wrap justify-between'>
+        {donneesFiltres.length > 0 ? (
+          donneesFiltres.map((donne) => (
+            <CardListen key={donne.id_histories} title={donne.title || "Titre non disponible"} description={donne.content || "Description non disponible"} />
+          ))
+        ) : (
+          <p>Aucune donnée à afficher.</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export function Read() {
+  const [data, setData] = useState([]);
+  const [donneesFiltres, setDonneesFiltres] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8001/Histories/Read");
+      if (response.data.status === "success") {
+        console.log("Résultats:", response.data.results[0]);
+        setData(response.data.results[0]);
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    setDonneesFiltres(data.filter((donne) => donne.types === "histoire"));
+  }, [data]);
+
+  return (
+    <div className='read my-5'>
+      <div className='flex flex-col md:flex-row justify-between items-center mb-4'>
+        <h3 className='font-medium text-xl md:text-2xl'>Read</h3>
+        <Link to="/ReadMore" className='flex items-center mt-2 md:mt-0 text-sm md:text-base'>
+          Voir plus <FaAngleRight className='ml-1' />
+        </Link>
+      </div>
+      <div className='p-2 card-vakiana flex flex-wrap justify-between'>
+        {donneesFiltres.length > 0 ? (
+          donneesFiltres.map((donne) => (
+            <CardRead key={donne.id_histories} title={donne.title || "Titre non disponible"} description={donne.content || "Description non disponible"} />
           ))
         ) : (
           <p>Aucune donnée à afficher.</p>
--- src/components/ListeComponent.jsx
+++ src/components/ListeComponent.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
-import List from './listModel.jsx'
-import images from "../assets/image/LOGO.png"
-import { PopUp } from './CreationStoryComponent.jsx'
+import React, { useState, useEffect } from 'react';
+import List from './listModel.jsx';
+import images from "../assets/image/LOGO.png";
+import axios from "axios";
+
 function ListComponent() {
+    const [data, setData] = useState([]);
+    const [donneesFiltres, setDonneesFiltres] = useState([]);
     const [afficheRead,setAfficheRead] = useState(true)
     const [afficheListen,setAfficheListen] = useState(false)
     const [Creer, SetCreer] = useState(false)
@@ -21,109 +24,110 @@ function ListComponent() {
         SetCreer(true)
     }
 
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("http://127.0.0.1:8001/Histories/Read");
+            if (response.data.status === "success") {
+                console.log("Résultats:", response.data.results[0]);
+                setData(response.data.results[0]);
+            }
+        } catch (error) {
+            console.error("Erreur lors de la requête:", error);
+        }
+    };
+
+    const getDateOnly = (dateString) => {
+        const date = new Date(dateString);
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
+
+        return date.toLocaleDateString('fr-FR', options);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        setDonneesFiltres(data.filter((donne) => donne.types === "histoire"));
+    }, [data]);
+
     return (
-    <div>
-        <div className='p-4 mx-36 flex items-center justify-between'>
-            <h1 className='font-bold text-3xl'>My contents</h1>
-            <div className='read'>
-                <button 
-                    onClick={CreerHistory}
-                    className='font-bold bg-black px-[1.7vw] py-[0.5vw] text-white hover:bg-slate-800 rounded-[10px]'>
-                        Create Story
+        <div>
+            <div className='p-4 mx-36 flex items-center justify-between'>
+                <h1 className='font-bold text-3xl'>My contents</h1>
+                <div className='read'>
+                    <button className='font-bold bg-black px-[1.7vw] py-[0.5vw] text-white hover:bg-slate-800 rounded-[10px]'>Create Story</button>
+                </div>
+            </div>
+            {
+                Creer && <PopUp></PopUp>
+            }
+
+            
+            <div className='p-4 mx-36 flex items-center'>
+                <button
+                    onClick={ListeRead} 
+                    className={
+                        `font-bold px-[1.7vw] py-[0.5vw]  rounded-t-[10px] 
+                        ${
+                        afficheRead ? 'bg-black text-white' : 'bg-white text-black hover:bg-gray-800 hover:text-white'}`}>
+                    
+                    Read(20)
+                </button>
+                
+                <button
+                    onClick={ListeListen} 
+                    className={
+                        `font-bold px-[1.7vw] py-[0.5vw] text-gray-700 rounded-t-[10px] 
+                        ${
+                        afficheListen ? 'bg-black text-white' : 'bg-white text-black hover:bg-gray-800 hover:text-white'}`}>
+    
+                    Listen(0)
                 </button>
             </div>
-        </div>
-        {
-        Creer &&
-            <PopUp></PopUp>
-        }
-
-
-
-
-
-
-
-        <div className='p-4 mx-36 flex items-center'>
-            <button
-                onClick={ListeRead} 
-                className={
-                    `font-bold px-[1.7vw] py-[0.5vw]  rounded-t-[10px] 
-                    ${
-                    afficheRead ? 'bg-black text-white' : 'bg-white text-black hover:bg-gray-800 hover:text-white'}`}>
-                  
-                Read(20)
-            </button>
-            
-            <button
-                onClick={ListeListen} 
-                className={
-                    `font-bold px-[1.7vw] py-[0.5vw] text-gray-700 rounded-t-[10px] 
-                    ${
-                    afficheListen ? 'bg-black text-white' : 'bg-white text-black hover:bg-gray-800 hover:text-white'}`}>
-   
-                Listen(0)
-            </button>
-        </div>
-
-
-        <div className='w-screen flex justify-center items-center'>
-            <hr className='border-[1px] w-[83.3vw] border-black'/>
-        </div>
-
-        <div className='p-4 mx-36 flex items-center justify-between'>
-            <div className='flex'>
-                <h1 className='px-[2vw] font-bold'>#</h1>
-                <h1 className='px-[2vw] font-bold'>Cover</h1>
-                <h1 className='px-[2vw] font-bold'>Title</h1>
+            <div className='w-screen flex justify-center items-center'>
+                <hr className='border-[1px] w-[83.3vw] border-black' />
             </div>
-            <div className='flex'>
-                <h1 className='px-[2vw] font-bold'>Genre</h1>
-                <h1 className='px-[2vw] font-bold'>Views</h1>
-                <h1 className='px-[2vw] font-bold'>Status</h1>
-                <h1 className='px-[2vw] font-bold'>Updated date</h1>
+            <div className='p-4 mx-36 flex items-center justify-between'>
+                <div className='flex'>
+                    <h1 className='px-[2vw] font-bold'>#</h1>
+                    <h1 className='px-[2vw] font-bold'>Cover</h1>
+                    <h1 className='px-[2vw] font-bold'>Title</h1>
+                </div>
+                <div className='flex'>
+                    <h1 className='px-[2vw] font-bold'>Genre</h1>
+                    <h1 className='px-[2vw] font-bold'>Views</h1>
+                    <h1 className='px-[2vw] font-bold'>Status</h1>
+                    <h1 className='px-[2vw] font-bold'>Updated date</h1>
+                </div>
             </div>
+            <div className='w-screen flex justify-center items-center'>
+                <hr className='border-[1px] w-[83.3vw] border-black' />
+            </div>
+            {
+                donneesFiltres.length > 0 ? (
+                    donneesFiltres.map((donne, index) => (
+                        <List
+                            key={donne.id_histories}
+                            id={index}
+                            image={images}
+                            Titre={donne.title || "Titre non disponible"}
+                            genre={donne.genre || "genre non diponible"}
+                            views={"+250 views"}
+                            status={"published"}
+                            date={getDateOnly(donne.created_at)} // Formater la date ici
+                        />
+                    ))
+                ) : (
+                    <p>Aucune donnée à afficher.</p>
+                )
+            }
         </div>
-
-        <div className='w-screen flex justify-center items-center'>
-            <hr className='border-[1px] w-[83.3vw] border-black'/>
-        </div>
-
-        {afficheRead && 
-        <div>
-
-        
-        <List
-            id="1"
-            image={images}
-            Titre="Dahalo"
-            genre="Erotique"
-            views="50 views"
-            status="published"
-            date="12 fevrier 2020"
-        />
-                <List
-            id="1"
-            image={images}
-            Titre="Dahalo"
-            genre="Erotique"
-            views="50 views"
-            status="published"
-            date="12 fevrier 2020"
-        />
-                <List
-            id="1"
-            image={images}
-            Titre="Dahalo"
-            genre="Erotique"
-            views="50 views"
-            status="published"
-            date="12 fevrier 2020"
-        />
-        </div>
-    }
-    </div>
-  )
+    );
 }
 
-export default ListComponent
+export default ListComponent;
