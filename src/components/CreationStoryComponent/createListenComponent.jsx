58606ee6eea56bdd761e5b547b4caab14c1573ec
ordkiki <onjaniaina235497@gmail.com>
2024-10-28 13:31:51 +0300

    Create

--- src/components/CreationStoryComponent/createListenComponent.jsx
+++ src/components/CreationStoryComponent/createListenComponent.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
-
-function createListenComponent() {
+import { PathCollection } from '../PathBrowse'
+function CreateListenComponent() {
   return (
-    <div>createListenComponent</div>
+    <div>
+        <PathCollection path="Create written Component"/>
+        <div className='mx-52 p-10'>
+            <div className='flex justify-between'>
+                <h4 className='text-3xl font-bold'>
+                    Create written component
+                </h4>
+                <button className='bg-black px-3 font-medium text-white'>
+                    Save
+                </button>
+            </div>
+            <form action="" method="POST" className="my-5" enctype="multipart/form-data">
+                <table>
+                    <tr>
+  
+                        <td>
+                            <input 
+                                className="w-[71vw] h-[20vh] bg-gray-100 p-2 rounded border-dashed border-[2px] border-black"  
+                                type="file" 
+                                name="file_img"
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="text" placeholder='title' name='titre' className="my-2 border w-[71vw] p-2"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="genre" placeholder='genre' name='genre' className="my-2 border w-[71vw] p-2"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="text" placeholder='votre text' name='text' className="my-2 border w-[71vw] p-2"/>
+                        </td>
+                    </tr>
+                </table>
+            </form> 
+        </div>
+    </div>
+
   )
 }
 
-export default createListenComponent
+export default CreateListenComponent
--- src/views/CreateListen.jsx
+++ src/views/CreateListen.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import CreateListenComponent from '../components/CreationStoryComponent/createListenComponent'
 function CreateListen() {
   return (
     <div>
         <Navbar></Navbar>
+        <CreateListenComponent/>
     </div>
   )
 }
