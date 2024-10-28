16fcb7cd54948da4e9ce4336d2aae6195701e769
ZouFitahiana <129894768+Fitahiana-herizo-RAKOTOMAMPIONONA@users.noreply.github.com>
2024-10-28 08:10:58 +0300

    avec dotenv & my content dynamique

--- src/components/ListeComponent.jsx
+++ src/components/ListeComponent.jsx
@@ -6,7 +6,8 @@ import { PopUp } from './CreationStoryComponent.jsx';
 
 function ListComponent() {
     const [data, setData] = useState([]);
-    const [donneesFiltres, setDonneesFiltres] = useState([]);
+    const [readDonne, setreadDonne] = useState([]);
+    const [listenDonne, setlistenDonne] = useState([]);
     const [afficheRead, setAfficheRead] = useState(true);
     const [afficheListen, setAfficheListen] = useState(false);
     const [isPopupVisible, setIsPopupVisible] = useState(false);
@@ -30,8 +31,10 @@ function ListComponent() {
     }
 
     const fetchData = async () => {
+        //mbola  tsy tena izy
+        const url = import.meta.env.VITE_API_URL + "/Read"
         try {
-            const response = await axios.get("http://127.0.0.1:8001/Histories/Read");
+            const response = await axios.get(url);
             if (response.data.status === "success") {
                 setData(response.data.results[0]);
             }
@@ -55,7 +58,10 @@ function ListComponent() {
     }, []);
 
     useEffect(() => {
-        setDonneesFiltres(data.filter((donne) => donne.types === "histoire"));
+        setreadDonne(data.filter((donne) => donne.types === "histoire"));
+    }, [data]);
+    useEffect(() => {
+        setlistenDonne(data.filter((donne) => donne.types === "audio"));
     }, [data]);
 
     return (
@@ -74,12 +80,12 @@ function ListComponent() {
                 <button
                     onClick={ListeRead} 
                     className={`font-bold px-[1.7vw] py-[0.5vw] rounded-t-[10px] ${afficheRead ? 'bg-black text-white' : 'bg-white text-black hover:bg-gray-800 hover:text-white'}`}>
-                    Read(20)
+                    Read({setreadDonne.length+1})
                 </button>
                 <button
                     onClick={ListeListen} 
                     className={`font-bold px-[1.7vw] py-[0.5vw] text-gray-700 rounded-t-[10px] ${afficheListen ? 'bg-black text-white' : 'bg-white text-black hover:bg-gray-800 hover:text-white'}`}>
-                    Listen(0)
+                    Listen({listenDonne.length+1})
                 </button>
             </div>
 
@@ -104,23 +110,35 @@ function ListComponent() {
             <div className='w-screen flex justify-center items-center'>
                 <hr className='border-[1px] w-[83.3vw] border-black' />
             </div>
-
-            {donneesFiltres.length > 0 ? (
-                donneesFiltres.map((donne, index) => (
-                    <List
-                        key={donne.id_histories}
-                        id={index}
-                        image={images}
-                        Titre={donne.title || "Titre non disponible"}
-                        genre={donne.genre || "Genre non disponible"}
-                        views={"+250 views"}
-                        status={"published"}
-                        date={getDateOnly(donne.created_at)}
-                    />
-                ))
-            ) : (
-                <p>Aucune donnée à afficher.</p>
-            )}
+            {
+                afficheRead ? 
+                    readDonne.length > 0 ? 
+                    readDonne.map((donne, index) => (
+                        <List
+                            key={donne.id_histories}
+                            id={index}
+                            image={images}
+                            Titre={donne.title || "Titre non disponible"}
+                            genre={donne.types || "Genre non disponible"}
+                            views={"+250 views"}
+                            status={"published"}
+                            date={getDateOnly(donne.created_at)}
+                        />
+                    )):<p>Aucune donnée à afficher 5 (READ).</p>
+                :listenDonne.length > 0 ? 
+                    listenDonne.map((donne, index) => (
+                        <List
+                            key={donne.id_histories}
+                            id={index}
+                            image={images}
+                            Titre={donne.title || "Titre non disponible"}
+                            genre={donne.types || "Genre non disponible"}
+                            views={"+250 views"}
+                            status={"published"}
+                            date={getDateOnly(donne.created_at)}
+                        />
+                    )):<p>Aucune donnée à afficher 5 (READ).</p>
+            }
         </div>
     );
 }
--- src/views/ReadMore.jsx
+++ src/views/ReadMore.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import { CardRead } from '../components/CardComponent'
 import PathBrowse from '../components/PathBrowse'
@@ -7,60 +7,88 @@ import axios from 'axios'
 import { Read } from '../components/BrowseComponent'
 
 export default function ReadMore() {
-  
+  const [data, setData] = useState([]);
+  const [donneesFiltres, setDonneesFiltres] = useState([]);
+
+  const fetchData = async () => {
+    const url = import.meta.env.VITE_API_URL + "/Read"
+    try {
+      const response = await axios.get(url);
+      if (response.data.status === "success") {
+        console.log("Résultats:", response.data.results[0]);
+        setData(response.data.results[0]);
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    setDonneesFiltres(data.filter((donne) => donne.types === "histoire"));
+  }, [data]);
+
   return (
     <div>
-        <Navbar></Navbar>
-        <div>
-          <PathBrowse path="Read"></PathBrowse>
-        </div>
-        <div className='flex justify-between px-40 my-5'>
-          <div className=''>
-            <div>
-              <h1 className='text-3xl font-bold'>
-                Read Stories
-              </h1>
-              <p>120 writtenContent Fetched</p>
-            </div>
-
-            <div>
-
-            </div>
+      <Navbar></Navbar>
+      <div>
+        <PathBrowse path="Read"></PathBrowse>
+      </div>
+      <div className='flex justify-between px-40 my-5'>
+        <div className=''>
+          <div>
+            <h1 className='text-3xl font-bold'>
+              Read Stories
+            </h1>
+            <p>120 writtenContent Fetched</p>
           </div>
 
-          
+          <div>
+
+          </div>
+        </div>
 
 
-        </div>
 
-        <div className='flex mx-40 p-4 w-[85vw]'>
 
-          
-        </div>
-        <div className='flex mx-40 p-4 w-[85vw]'>
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-        </div>
-        <div className='flex mx-40 p-4 w-[85vw]'>
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-        </div>
-        <div className='flex mx-40 p-4 w-[85vw]'>
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-        </div>
-        <div className='flex mx-40 p-4 w-[85vw]'>
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-          <CardRead title="Angatra" description="tantara mapatahoatra" />
-        </div>
+      </div>
+
+      <div className='flex mx-40 p-4 w-[85vw]'>
+        {donneesFiltres.length > 0 ? (
+          donneesFiltres.map((donne) => (
+            <CardRead key={donne.id_histories} title={donne.title || "Titre non disponible"} description={donne.content || "Description non disponible"} />
+          ))
+        ) : (
+          <p>Aucune donnée à afficher.</p>
+        )}
+      </div>
+      <div className='flex mx-40 p-4 w-[85vw]'>
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+      </div>
+      <div className='flex mx-40 p-4 w-[85vw]'>
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+      </div>
+      <div className='flex mx-40 p-4 w-[85vw]'>
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+      </div>
+      <div className='flex  wrap mx-40 p-4 w-[85vw]'>
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+        <CardRead title="Angatra" description="tantara mapatahoatra" />
+      </div>
 
     </div>
   )
