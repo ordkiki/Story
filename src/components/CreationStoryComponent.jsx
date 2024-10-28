225342c74233f4185ee47c14d9e63bc028f3f357
farattiana <faratianarazafindrabe@gmail.com>
2024-10-28 13:52:51 +0300

    update marge on modal creation

--- src/components/CreationStoryComponent.jsx
+++ src/components/CreationStoryComponent.jsx
@@ -20,24 +20,22 @@ export function PopUp({ onClose }) {
     }
 
     return (
-        <div className='top-0 bg-[rgba(0,0,0,0.2)] w-screen h-screen flex items-center justify-center absolute'>
-            <div className='relative p-10 w-[40vw] h-[70vh] bg-white border rounded-[20px]'>
-                <h3 className='text-xl font-medium'>Create history</h3>
+        <div className='top-0 bg-[rgba(0,0,0,0.2)] w-screen h-full flex items-center justify-center absolute'>
+            <div className='relative p-10 w-max h-max bg-white border rounded-[20px]'>
+                {/* <h3 className='text-xl font-medium'>Create history</h3> */}
                 <button onClick={onClose} className='absolute right-10 top-10'><FaTimes /></button>
-                <div className='p-20'>
-                    <h2 className='text-center text-2xl font-bold'>Show Us your creativity</h2>
+                <div className='flex flex-col gap-4'>
+                    <h2 className='text-2xl font-bold text-center'>Show Us your creativity</h2>
                     <p className='text-center text-gray-400'>Choose the type of Story you want</p>
-                    <div className='flex justify-between'>
+                    <div className='flex items-center justify-between gap-x-10 h-max'>
                         <div 
                             onClick={ChoisirLire}
                             className={`font-bold cursor-pointer ${Lire ? 'border-black border-[3px]' : 'hover:border-gray-800'}`}
                         >
-                            <div className='rounded cursor-pointer w-[10vw] h-[25vh] bg-gray-200 flex justify-center items-center'>
-                                <div>
-                                    <FaBookOpen className='text-2xl text-gray-800' />
-                                    <h4 className='font-bold text-center'>Read</h4>
-                                    <p>For written content</p>
-                                </div>
+                            <div className='flex flex-col items-center justify-center h-32 px-4 bg-gray-200 rounded cursor-pointer w-max'>
+                                <FaBookOpen className='text-2xl text-gray-800' />
+                                <h4 className='font-bold text-center'>Read</h4>
+                                <p>For written content</p>
                             </div>
                         </div>
 
@@ -45,19 +43,17 @@ export function PopUp({ onClose }) {
                             onClick={ChoisirAudio}
                             className={`font-bold cursor-pointer ${likeAudio ? 'border-black border-[3px]' : 'hover:border-gray-800'}`}
                         >
-                            <div className='rounded w-[10vw] h-[25vh] bg-gray-200 flex justify-center items-center'>
-                                <div>
-                                    <FaHeadphones className='text-2xl text-gray-800' />
-                                    <h4 className='font-bold text-center'>Listen</h4>
-                                    <p>For audio content</p>
-                                </div>
+                            <div className='flex flex-col items-center justify-center h-32 px-4 bg-gray-200 rounded cursor-pointer w-max'>
+                                <FaHeadphones className='text-2xl text-gray-800' />
+                                <h4 className='font-bold text-center'>Read</h4>
+                                <p>For written content</p>
                             </div>
                         </div>
                     </div>
 
-                    <div className='my-10 flex items-center justify-center'>
+                    <div className='flex items-center justify-center my-10'>
                         <button className='hover:bg-black px-10 rounded-[20px] hover:text-white transition-all text-center p-2 border-[3px] border-black'>
-                            <Link to={`/Collection/${routerPath}`} className='items-center flex'>
+                            <Link to={`/Collection/${routerPath}`} className='flex items-center'>
                                 Next <FaAngleRight />
                             </Link>
                         </button>
