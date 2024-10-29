dd1eb66fe4d6c612eaa4f668bbfbf2925e25a888
farattiana <faratianarazafindrabe@gmail.com>
2024-10-29 13:13:46 +0300

    upfate button style

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -15,13 +15,13 @@ export function CardRead(props) {
     <div className='h-auto w-full md:w-[30%] lg:w-[22%] m-4'>
       <div className='relative rounded-xl bg-gray-200 shadow-md p-4 flex flex-col justify-between h-[25vh]'>
 
-        <FaBookmark className="m-1 text-xl absolute top-0 right-0" />
+        <FaBookmark className="absolute top-0 right-0 m-1 text-xl" />
       </div>
-      <h3 className='font-semibold text-lg'>{props.title}</h3>
-      <p className='text-gray-500 font-thin'>{props.description}</p>
+      <h3 className='text-lg font-semibold'>{props.title}</h3>
+      <p className='font-thin text-gray-500'>{props.description}</p>
       <button 
         onClick={changerPage}
-        className='bg-gray-400 px-4 text-white font-bold rounded hover:scale-105'
+        className='px-4 py-2 font-bold text-white bg-black rounded hover:scale-105'
       >
         Read
       </button>
@@ -34,13 +34,13 @@ export function CardListen(props) {
   return (
     <div className='h-auto w-full md:w-[30%] lg:w-[22%] mx-2 my-4'>
       <div className='relative rounded-xl bg-gray-200 shadow-md p-4 flex flex-col justify-between h-[25vh]'>
-        <FaBookmark className="m-1 text-xl absolute top-0 right-0" />
+        <FaBookmark className="absolute top-0 right-0 m-1 text-xl" />
         <button className='p-2 text-white absolute right-2 bottom-2 rounded-[50%] border-white border border-solid'>
           <FaPlay />
         </button>
       </div>
-      <h3 className='font-semibold text-lg'>{props.title}</h3>
-      <p className='text-gray-500 font-thin'>{props.description}</p>
+      <h3 className='text-lg font-semibold'>{props.title}</h3>
+      <p className='font-thin text-gray-500'>{props.description}</p>
     </div>
   );
 }
